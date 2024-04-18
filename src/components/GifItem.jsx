38630342ce3c8e971460e8f6675b1565dcf0f99c
Carlos <carlos.mancilla993@gmail.com>
2024-04-18 07:35:51 -0500

    Primer commit

---
+++ src/components/GifItem.jsx
@@ -0,0 +1,15 @@
+import propTypes from 'prop-types';
+
+export const GifItem = ({ title, url }) => {
+    return (
+        <div className="card">
+            <img src={url} alt={title} />
+            <p>{title}</p>
+        </div>
+    );
+};
+
+GifItem.propTypes = {
+    title: propTypes.string.isRequired,
+    url: propTypes.string.isRequired,
+};
